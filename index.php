e9f65095ba47ab4ff5469c9ae8def4355204373a
Liepinsh <37236365+Liepinsh@users.noreply.github.com>
2018-05-11 08:35:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,5 @@ if(!isset($_SESSION["location_state"])) {
     $_SESSION["location_state"] = 'login';
 }
 
-
-// // header
-// require_once 'components/header.php';
-
 //content
 require_once 'components/index.php';
-
-// //footer
-// require_once 'components/footer.php';
